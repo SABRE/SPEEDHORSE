8c9c0932224e87c631d5f4fbcdde72a9d839033b
pralayanima <pralay.anima@gmail.com>
2011-06-14 00:48:35 +0530

    Fifth Commit

--- settings/override/forum.ini.append.php~
+++
@@ -1,5 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[ForumSettings]
-StickyUserGroupArray[]=12
-*/ ?>
--- settings/siteaccess/ezflow_site_admin/design.ini.append.php
+++
@@ -1,6 +0,0 @@
-<?php /* #?ini charset="utf8"?
-
-[StylesheetSettings]
-SiteCSS=var/storage/packages/eZ-systems/ezflow_design/files/default/file/site-colors.css
-ClassesCSS=var/storage/packages/eZ-systems/ezflow_design/files/default/file/classes-colors.css
-*/ ?>
