4c59080f70749867dfe8e6de0bf0313390e11da3
pralayanima <pralay.anima@gmail.com>
2011-06-24 19:38:45 +0530

    New changes

---
+++ extension/ezuser/settings/site.ini.append.php
@@ -0,0 +1,7 @@
+<?php /* #?ini charset="utf-8"?
+[RoleSettings]
+PolicyOmitList[]=userdetail/add
+PolicyOmitList[]=userdetail/list
+PolicyOmitList[]=userdetail/fparticle
+PolicyOmitList[]=userdetail/racingnews
+*/ ?>
