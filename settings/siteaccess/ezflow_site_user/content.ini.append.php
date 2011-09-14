1b1c291acc534562186a164d57b633a2f187ef1c
pralayanima <pralay.anima@gmail.com>
2011-09-15 03:01:59 +0530

    New Changes

---
+++ settings/siteaccess/ezflow_site_user/content.ini.append.php
@@ -0,0 +1,12 @@
+<?php /* #?ini charset="utf-8"?
+
+[VersionView]
+AvailableSiteDesignList[]=ezflow
+
+[ObjectRelationDataTypeSettings]
+ClassAttributeStartNode[]=236;AddRelatedBannerImageToDataType
+
+[UnpublishSettings]
+RootNodeList[]=244
+ClassList[]=16
+*/ ?>
---
+++ settings/siteaccess/ezflow_site_user/design.ini.append.php
@@ -0,0 +1,13 @@
+<?php /* #?ini charset="utf-8"?
+
+[StylesheetSettings]
+#SiteCSS=var/storage/packages/eZ-systems/ezflow_design/files/default/file/site-colors.css
+#ClassesCSS=var/storage/packages/eZ-systems/ezflow_design/files/default/file/classes-colors.css
+CSSFileList[]
+
+[JavaScriptSettings]
+JavaScriptList[]=insertmedia.js
+JavaScriptList[]=speedhorse.js
+JavaScriptList[]=slider.js
+JavaScriptList[]=jquery.jcarousel.min.js
+*/ ?>
